984c478b1c64f98522a70dc1adae9b6bcbe1dc32
dboldt7 <69984918+dboldt7@users.noreply.github.com>
2020-09-01 19:41:57 -0500

    Add files via upload

---
+++ revisedCredit2.py
@@ -0,0 +1,11 @@
+
+credit_score = int(input("What is your credit score? "))
+
+if credit_score >= 720:
+    print("Your Credit is Excellent!")
+elif credit_score >= 690:
+    print("Your Credit is Good.")
+elif credit_score >= 630:
+    print("Your Credit is Fair")
+else:
+    print("Your Credit is Bad")
