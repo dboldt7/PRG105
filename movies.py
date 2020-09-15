41ec151802baa946e9dca7c57ef8e6ae347acdad
dboldt7 <69984918+dboldt7@users.noreply.github.com>
2020-09-15 11:56:37 -0500

    Add files via upload

---
+++ movies.py
@@ -0,0 +1,39 @@
+print("Please select a movie from below")
+print("A. The Godfather")
+print("B. Wall-E")
+print("C. The Martian")
+print("D. Pulp Fiction")
+print("E. Jaws")
+letter = input("Enter the letter of your choice. ")
+
+
+def a():
+    print("This movie is rated R and is playing on screen 12")
+
+
+def b():
+    print("This movie rated G and is playing on screen 19")
+
+
+def c():
+    print("This movie is rated PG-13 and is playing on screen 4")
+
+
+def d():
+    print("This movie is rated R and is playing on screen 1")
+
+
+def e():
+    print("This movie rated PG and is playing on screen 7")
+
+
+if letter == "A":
+    a()
+elif letter == "B":
+    b()
+elif letter == "C":
+    c()
+elif letter == "D":
+    d()
+else:
+    e()
