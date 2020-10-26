700f1f8fee6a8073cf8eb7a682acea8efe71db0e
dboldt7 <69984918+dboldt7@users.noreply.github.com>
2020-10-25 21:42:38 -0500

    Add files via upload

--- 8.3 Assignment/8.3 Program.py
+++ 8.3 Assignment/8.3 Program.py
@@ -1,37 +1,34 @@
 accounts_list = []
 over_list = []
 
-try:
-    file1 = open('accounts.txt', 'r')
-except IOError:
-    print("There was an error in opening this file")
-    while not accounts_list:
-        file1 = input("Restart the program and try again")
 
-line = file1.readline()
-while line != '':
-    accounts_list.append(line.rstrip('\n'))
-    line = file1.readline()
+def main_func():
+    try:
+        file1 = open('accounts.txt', 'r')
 
-file1.close()
+        line = file1.readline()
+        while line != '':
+            accounts_list.append(line.rstrip('\n'))
+            line = file1.readline()
 
-try:
-    file2 = open('over90.txt', 'r')
-except IOError:
-    print("There was an error in opening a file")
-    while not over_list:
-        var = input("Restart the program and try again")
+        file1.close()
 
-line = file2.readline()
-while line != '':
-    over_list.append(line.rstrip('\n'))
-    line = file2.readline()
+        file2 = open('over90.txt', 'r')
+        line = file2.readline()
+        while line != '':
+            over_list.append(line.rstrip('\n'))
+            line = file2.readline()
 
-file2.close()
+        file2.close()
 
-print("The following accounts are at least 90 days overdue:\n")
+        print("The following accounts are at least 90 days overdue:\n")
 
-for account in accounts_list:
-    for code in over_list:
-        if code in account:
-            print(account)
+        for account in accounts_list:
+            for code in over_list:
+                if code in account:
+                    print(account)
+    except IOError:
+        print("There was a problem opening a file, restart and try again.")
+
+
+main_func()
